6f8150d10fb976d9c19e3df6e139f8c9a479a3a8
aditya26-dev <dewaa.kadek26@gmail.com>
2020-10-30 12:27:21 +0800

    new again

--- UPMSITE_Kelompok01/settings.py
+++ UPMSITE_Kelompok01/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR / 'static')
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'static' / 'media'
